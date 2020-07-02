a62aa0197f202c0f248da28f04e26b60d4885fa8
Minh-Tri Pham <pmtri80@gmail.com>
2020-07-02 02:44:58 +0100

    v0.0.5: better support for mtdoc

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_namespace_packages
 from mt.opencv.version import version
 
-setup(name='opencvmt',
+setup(name='mtopencv',
       version=version,
       description="Minh-Tri Pham's extra modules using OpenCV",
       author=["Minh-Tri Pham"],
-      packages=find_packages() + find_namespace_packages(include=['mt.*']),
+      packages=find_namespace_packages(include=['mt.*']),
       install_requires=[
-          'basemt>=0.3.3',
+          'basemt>=0.4.1',
           # opencv is detected at run-time for flexibility
       ]
       )
