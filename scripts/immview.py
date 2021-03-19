d7fac839db8f4d68c74e2d67dd8b4bdeb6eb89ab
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-03-19 07:45:27 +0000

    v0.1.3: immview now can deal with many image file types.

--- scripts/immview.py
+++ scripts/immview.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 
 import argparse
-
+from imghdr import what
 from mt import cv, np
 
 def main(args):
-    imm = cv.immload(args.imm_file)
-    print("Image path: {}".format(args.imm_file))
-    print("Pixel format: {}".format(imm.pixel_format))
-    print("Meta:")
-    print(imm.meta)
-    print("Press any key to exit.")
-    cv.namedWindow('image')
-    cv.imshow('image', imm.image)
-    cv.waitKey(0)
+    if args.imm_file.endswith('.imm'):
+        imm = cv.immload(args.imm_file)
+        print("Image path: {}".format(args.imm_file))
+        print("Pixel format: {}".format(imm.pixel_format))
+        print("Meta:")
+        print(imm.meta)
+        print("Press any key to exit.")
+        cv.namedWindow('image')
+        cv.imshow('image', imm.image)
+        cv.waitKey(0)
+    else:
+        image_type = what(args.imm_file)
+        if image_type is None:
+            print("Not an image file: {}".format(args.imm_file))
+        else:
+            print("Image path: {}".format(args.imm_file))
+            print("File type: {}".format(image_type))
+            image = cv.imread(args.imm_file)
+            print("Press any key to exit.")
+            cv.namedWindow('image')
+            cv.imshow('image', image)
+            cv.waitKey(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Tool to view an image with metadata (IMM) file.")
