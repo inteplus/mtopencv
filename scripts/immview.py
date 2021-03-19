bf907f9992c407ff7f5b4f1769756c7c4a54c8d4
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-03-19 14:29:08 +0000

    now able to view 'rgba' and 'bgra' image files

--- scripts/immview.py
+++ scripts/immview.py
@@ -4,6 +4,43 @@ import argparse
 from imghdr import what
 from mt import cv, np
 
+def get_image(imm):
+    if imm.pixel_format in ['gray', 'bgr']:
+        return imm.image
+    
+    if imm.pixel_format == 'rgb':
+        return np.ascontiguousarray(np.flip(imm.image, axis=-1))
+    
+    if imm.pixel_format == 'rgba':
+        h, w = imm.image.shape[:2]
+        image = np.zeros((h*2, w*2, 3), dtype=np.uint8)
+        image[h:h*2,:w,0] = imm.image[:,:,2]
+        image[h:h*2,:w,1] = imm.image[:,:,1]
+        image[h:h*2,:w,2] = imm.image[:,:,0]
+
+        image[:h,w:w*2,0] = imm.image[:,:,3]
+
+        image[:h,:w,0] = np.round(imm.image[:,:,2].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        image[:h,:w,1] = np.round(imm.image[:,:,1].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        image[:h,:w,2] = np.round(imm.image[:,:,0].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        return image
+
+    if imm.pixel_format == 'rgba':
+        h, w = imm.image.shape[:2]
+        image = np.zeros((h*2, w*2, 3), dtype=np.uint8)
+        image[h:h*2,:w,0] = imm.image[:,:,0]
+        image[h:h*2,:w,1] = imm.image[:,:,1]
+        image[h:h*2,:w,2] = imm.image[:,:,2]
+
+        image[:h,w:w*2,0] = imm.image[:,:,3]
+
+        image[:h,:w,0] = np.round(imm.image[:,:,0].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        image[:h,:w,1] = np.round(imm.image[:,:,1].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        image[:h,:w,2] = np.round(imm.image[:,:,2].astype(float)*imm.image[:,:,3].astype(float)/255).astype(np.uint8)
+        return image
+
+    raise ValueError("Imm with pixel format '{}' is not supported.".format(imm.pixel_format))
+
 def view(image, max_width=640):
     if max_width < image.shape[1]:
         height = image.shape[0]*max_width//image.shape[1]
@@ -21,7 +58,7 @@ def main(args):
         print("Resolution: {}x{}".format(imm.image.shape[1], imm.image.shape[0]))
         print("Meta:")
         print(imm.meta)
-        view(imm.image, max_width=args.max_width)
+        view(get_image(imm), max_width=args.max_width)
     else:
         image_type = what(args.imm_file)
         if image_type is None:
