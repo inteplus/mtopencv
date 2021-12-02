5116bc8dc103b88a971df06ed9afb5d3ad40b170
Minh-Tri Pham <pmtri80@gmail.com>
2021-12-02 13:12:15 +0000

    v1.2: fix for opencv-python 4.5.4

--- mt/cv/__init__.py
+++ mt/cv/__init__.py
@@ -1,7 +1,10 @@
+import cv2
+for key in cv2.__dict__:
+    if not key.startswith('__') and not key == 'cv2':
+        globals()[key] = getattr(cv2, key)
+from cv2 import __version__
 from mt.opencv import cv2, logger
-from cv2 import *
 from mt.opencv.polygon import *
 from mt.opencv.warping import *
 from mt.opencv.image import *
 from mt.opencv.ansi import *
-__version__ = cv2.__version__
